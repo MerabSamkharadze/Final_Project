e24654f991eb68256b71ae0223682ad55cb752fd
Merabi Samkharadze <samkharadzemerab@gmail.com>
2024-06-30 13:19:45 +0400

    add real movie api "

--- script.js
+++ script.js
@@ -2,8 +2,10 @@
 
 const getData = async () => {
   try {
-    const data = await axios.get("https://reqres.in/api/users?");
-    console.log(data.data.data);
+    const data = await axios.get(
+      "http://www.omdbapi.com/?i=tt3896198&apikey=2386a7bd"
+    );
+    console.log(data);
   } catch (err) {
     const h1 = document.createElement("h1");
     h1.textContent = ` Server error : ${err}`;
