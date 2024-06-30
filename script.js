6e0d2742c6962807b8f23df65c769109ea931351
Merabi Samkharadze <samkharadzemerab@gmail.com>
2024-06-30 14:31:28 +0400

    add search input and logic

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 "use strict";
-
-const getData = async () => {
+const search_movie_input = document.getElementById("search_movie");
+const getData = async (searchTerm) => {
   try {
-    const data = await axios.get(
-      "http://www.omdbapi.com/?i=tt3896198&apikey=2386a7bd"
-    );
-    console.log(data);
+    const response = await axios.get("http://www.omdbapi.com/", {
+      params: {
+        apikey: "2386a7bd",
+        s: searchTerm,
+      },
+    });
+    console.log(response.data);
   } catch (err) {
     const h1 = document.createElement("h1");
     h1.textContent = ` Server error : ${err}`;
@@ -14,3 +17,13 @@ const getData = async () => {
 };
 
 getData();
+let timeOutId;
+const onInput = (event) => {
+  if (timeOutId) {
+    clearTimeout(timeOutId);
+  }
+  timeOutId = setTimeout(() => {
+    getData(event.target.value);
+  }, 1000);
+};
+search_movie_input.addEventListener("input", onInput);
