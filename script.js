93217561b59457f91bc1300f9a214dae9dd0a80e
Merabi Samkharadze <samkharadzemerab@gmail.com>
2024-07-04 13:27:26 +0400

    add export/import methods for two functions(getData() and onMovieSelect() )

--- script.js
+++ script.js
@@ -1,26 +1,6 @@
 "use strict";
-import { debounce } from "./utils.js";
+import { debounce, getData, onMovieSelect } from "./utils.js";
 
-const getData = async (searchTerm) => {
-  try {
-    const response = await axios.get("http://www.omdbapi.com/", {
-      params: {
-        apikey: "2386a7bd",
-        s: searchTerm,
-      },
-    });
-
-    if (response.data.Error) {
-      return [];
-    }
-
-    return response.data.Search;
-  } catch (err) {
-    const h1 = document.createElement("h1");
-    h1.textContent = ` Server error : ${err}`;
-    document.body.appendChild(h1);
-  }
-};
 // search
 const root = document.querySelector(".autocomplete");
 root.innerHTML = `
@@ -73,6 +53,8 @@ logo.addEventListener("click", () => {
     top: 0,
     behavior: "smooth",
   });
+  input.value = "";
+  onInput();
 });
 
 document.addEventListener("click", function (e) {
@@ -80,45 +62,3 @@ document.addEventListener("click", function (e) {
     dropdown.classList.remove("is-active");
   }
 });
-
-// movie-Content
-const movie_section = document.getElementById("movie_section");
-movie_section.innerHTML = "";
-const onMovieSelect = async (movie) => {
-  const response = await axios.get("http://www.omdbapi.com/", {
-    params: {
-      apikey: "2386a7bd",
-      i: movie.imdbID,
-    },
-  });
-
-  const movie_root = `
-
-<div class="container">
-<div class="posterDiv">
-  <img
-    src=${response.data.Poster}
-  />
-</div>
-<div class="contentDiv">
- 
-    <h2 class="content_movie_title">${response.data.Title}</h2>
-    <h2 class="imdb">IMDB: <span>${response.data.imdbRating}</span></h2>
-
-  <div class="">
-    <p>გამოშვების წელი: <span>${response.data.Year}</span></p>
-    <p>ქვეყანა: <span>${response.data.Country}</span></p>
-    <p>ჟანრი: <span>${response.data.Genre}</span></p>
-    <p>რეჟისორი: <span>${response.data.Director}</span></p>
-    <p>როლებში: <span>${response.data.Actors}</span></p>
-    <p>
-      აღწერა:
-      <span
-        >${response.data.Plot} </span
-      >
-    </p>
-  </div>
-</div>
-</div>`;
-  movie_section.innerHTML = movie_root;
-};
--- utils.js
+++ utils.js
@@ -11,3 +11,68 @@ export const debounce = (func, delay) => {
     }, delay);
   };
 };
+
+// movie-Content
+
+export const getData = async (searchTerm) => {
+  try {
+    const response = await axios.get("http://www.omdbapi.com/", {
+      params: {
+        apikey: "2386a7bd",
+        s: searchTerm,
+      },
+    });
+
+    if (response.data.Error) {
+      return [];
+    }
+
+    return response.data.Search;
+  } catch (err) {
+    const h1 = document.createElement("h1");
+    h1.textContent = ` Server error : ${err}`;
+    document.body.appendChild(h1);
+  }
+};
+
+const movie_section = document.getElementById("movie_section");
+movie_section.innerHTML = "";
+
+export const onMovieSelect = async (movie) => {
+  const response = await axios.get("http://www.omdbapi.com/", {
+    params: {
+      apikey: "2386a7bd",
+      i: movie.imdbID,
+    },
+  });
+
+  const movie_root = `
+
+<div class="container">
+<div class="posterDiv">
+  <img
+    src=${response.data.Poster}
+  />
+</div>
+<div class="contentDiv">
+ 
+    <h2 class="content_movie_title">${response.data.Title}</h2>
+    <h2 class="imdb">IMDB: <span>${response.data.imdbRating}</span></h2>
+
+  <div class="">
+    <p>გამოშვების წელი: <span>${response.data.Year}</span></p>
+    <p>ქვეყანა: <span>${response.data.Country}</span></p>
+    <p>ჟანრი: <span>${response.data.Genre}</span></p>
+    <p>რეჟისორი: <span>${response.data.Director}</span></p>
+    <p>როლებში: <span>${response.data.Actors}</span></p>
+    <p>
+      აღწერა:
+      <span
+        >${response.data.Plot} </span
+      >
+    </p>
+  </div>
+</div>
+</div>`;
+  movie_section.innerHTML = movie_root;
+};
