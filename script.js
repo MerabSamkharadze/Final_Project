07e8762c6837db9da98e37b58b5bec3c5cd5c7b0
Merabi Samkharadze <samkharadzemerab@gmail.com>
2024-07-04 13:31:38 +0400

    move onInput fnc from script.js to utils.js

--- script.js
+++ script.js
@@ -1,50 +1,7 @@
 "use strict";
-import { debounce, getData, onMovieSelect } from "./utils.js";
+import { debounce, onInput } from "./utils.js";
 
-// search
-const root = document.querySelector(".autocomplete");
-root.innerHTML = `
-      <input
-        type="text"
-        id="search_movie"
-        placeholder="Search Movie"
-      />
-      <div class="dropdown">
-        <div class="dropdown-menu">
-           <div class="dropdown-content results"></div>
-       </div>
-       </div>
-
-`;
 const input = document.querySelector("#search_movie");
-const dropdown = document.querySelector(".dropdown");
-const resultsWrapper = document.querySelector(".results");
-
-const onInput = async (event) => {
-  resultsWrapper.innerHTML = " ";
-
-  const movies = await getData(event.target.value);
-  if (!movies.length) {
-    dropdown.classList.remove("is-active");
-    return;
-  }
-  dropdown.classList.add("is-active");
-  movies.forEach((movie) => {
-    const option = document.createElement("a");
-    const imgSrc = movie.Poster === "N/A" ? " " : movie.Poster;
-    option.classList.add("dropdown-item");
-    option.innerHTML = `
-    <img src="${imgSrc}" />
-    ${movie.Title}
-  `;
-    option.addEventListener("click", () => {
-      dropdown.classList.remove("is-active");
-      input.value = movie.Title;
-      onMovieSelect(movie);
-    });
-    resultsWrapper.appendChild(option);
-  });
-};
 input.addEventListener("input", debounce(onInput, 500));
 
 const logo = document.getElementById("logo");
--- utils.js
+++ utils.js
@@ -76,3 +76,48 @@ export const onMovieSelect = async (movie) => {
 </div>`;
   movie_section.innerHTML = movie_root;
 };
+
+// search
+const root = document.querySelector(".autocomplete");
+root.innerHTML = `
+      <input
+        type="text"
+        id="search_movie"
+        placeholder="Search Movie"
+      />
+      <div class="dropdown">
+        <div class="dropdown-menu">
+           <div class="dropdown-content results"></div>
+       </div>
+       </div>
+
+`;
+const input = document.querySelector("#search_movie");
+const dropdown = document.querySelector(".dropdown");
+const resultsWrapper = document.querySelector(".results");
+
+export const onInput = async (event) => {
+  resultsWrapper.innerHTML = " ";
+
+  const movies = await getData(event.target.value);
+  if (!movies.length) {
+    dropdown.classList.remove("is-active");
+    return;
+  }
+  dropdown.classList.add("is-active");
+  movies.forEach((movie) => {
+    const option = document.createElement("a");
+    const imgSrc = movie.Poster === "N/A" ? " " : movie.Poster;
+    option.classList.add("dropdown-item");
+    option.innerHTML = `
+    <img src="${imgSrc}" />
+    ${movie.Title}
+  `;
+    option.addEventListener("click", () => {
+      dropdown.classList.remove("is-active");
+      input.value = movie.Title;
+      onMovieSelect(movie);
+    });
+    resultsWrapper.appendChild(option);
+  });
+};
