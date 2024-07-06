8381fb95241a27b8974e60d5b5d62001c542e1cb
Merabi Samkharadze <samkharadzemerab@gmail.com>
2024-07-06 21:18:38 +0400

    add movie_section

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 "use strict";
-import { debounce, onInput } from "./utils.js";
+import { debounce, onInput, getData } from "./utils.js";
 
 const input = document.querySelector("#search_movie");
 input.addEventListener("input", debounce(onInput, 500));
 
-// const logo = document.getElementById("logo");
-// logo.addEventListener("click", () => {
-//   window.scrollTo({
-//     top: 0,
-//     behavior: "smooth",
-//   });
-//   input.value = "";
-//   onInput(input.value);
-// });
+const logo = document.getElementById("logo");
+logo.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+  input.value = "";
+  onInput(input.value);
+});
 
 document.addEventListener("click", function (e) {
   if (!root.contains(e.target)) {
     dropdown.classList.remove("is-active");
   }
+});
+
+const movie_colections = await getData("avengers");
+movie_colections.forEach((element) => {
+  console.log(element);
 });
