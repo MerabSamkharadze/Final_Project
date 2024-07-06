896e335a824407eeab1cc10a87f28fa9c155d4ba
Merabi Samkharadze <samkharadzemerab@gmail.com>
2024-07-06 21:32:01 +0400

    add spiderman films on home page

--- script.js
+++ script.js
@@ -19,8 +19,19 @@ document.addEventListener("click", function (e) {
     dropdown.classList.remove("is-active");
   }
 });
-
-const movie_colections = await getData("avengers");
+const movie_colection = document.querySelector(".movie_colection");
+const movie_colections = await getData("Spider-man");
+const fragment = new DocumentFragment();
 movie_colections.forEach((element) => {
   console.log(element);
+  const movieDiv = document.createElement("div");
+  movieDiv.classList.add("movie");
+  const img = document.createElement("img");
+  img.src = element.Poster;
+  const title = document.createElement("h1");
+  title.textContent = element.Title;
+  movieDiv.appendChild(img);
+  movieDiv.appendChild(title);
+  fragment.appendChild(movieDiv);
 });
+movie_colection.appendChild(fragment);
